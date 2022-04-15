47812c1e83490a0b32e9bb85677ac6b323d50dac
Martin Allende <martin.allende.2002@gmail.com>
2022-04-15 17:31:41 -0300

    tp1 completo

---
+++ TP_[1]/src/funciones.h
@@ -0,0 +1,11 @@
+
+int mostrarOpciones(void);
+int utn_getInt(int *pResultado, char *mensaje, char *mensajeError,  int maximo, int minimo, int reintentos);
+int calcularCostos(int precioAerolineas, int precioLatam, int km, float *precioCreditoLatam, float *precioDebitoLatam, float *precioBitcoinLatam, float *precioUnitarioLatam, float *precioCreditoAero, float *precioDebitoAero, float *precioBitcoinAero, float *precioUnitarioAero, float *diferenciaDePrecio);
+int mostrarCostos(int precioAerolineas, int precioLatam, int km, float precioCreditoLatam, float precioDebitoLatam, float precioBitcoinLatam, float precioUnitarioLatam, float precioCreditoAero, float precioDebitoAero, float precioBitcoinAero, float precioUnitarioAero, float diferenciaDePrecio);
+
+
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+#endif /* FUNCIONES_H_ */
